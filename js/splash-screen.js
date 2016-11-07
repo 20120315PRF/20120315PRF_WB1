3453e7d0640c926c76e71e459fea81f7a0d49834
BCLM <luismiguel.louzao@louinc.es>
2016-11-08 00:03:20 +0100

    Make responsible with bootstrap

--- js/splash-screen.js
+++ js/splash-screen.js
@@ -1,11 +1,12 @@
 /**
     Waits until the main animation is finished to execute the sequence of animation
 */
+
 $('.navbar').removeClass('navbar-fixed-top');
 $(".circle-animated").bind("animationend webkitAnimationEnd oAnimationEnd MSAnimationEnd", function(){
-	/** fade in text */ 
+  //fade in text
   $('.fadeIn-text').fadeIn('fast', function(){
-    /** execute function after 3000 seconds */
+    // execute function after 3000 seconds
     setTimeout(function(){
     	$('#splash-screen').fadeOut('slow', function(){
             $('.navbar').addClass('navbar-fixed-top');
